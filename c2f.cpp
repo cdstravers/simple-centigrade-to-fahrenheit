df658b7ea27f6e05ed88ea80ac6dc3113628bba1
cdstravers <cdstravers@gmail.com>
2017-07-02 11:07:51 -0400

    Add files via upload

---
+++ c2f.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+int celcius;
+int fahrenheit;
+
+cout<<"enter celcius ";
+cin>>celcius;
+fahrenheit=((celcius*9)/5)+32;
+cout<<"fahrenheit is ";
+cout<<fahrenheit;
+cout<<"\n";
+return 0;
+}
